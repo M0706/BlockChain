bf8e1bf7855167111ab8b110cd397a4f4f173e45
Manav Garg <manavgarg@Manavs-MacBook-Air.local>
2021-05-01 21:33:11 +0530

    previous block function added

--- GeneralBlockChain/BlockChain.py
+++ GeneralBlockChain/BlockChain.py
@@ -13,7 +13,19 @@ class BlockChain:
         self.create_block(proof=1,previous_hash='0')
     
     def create_block(self,proof,previous_hash):
+        block={'index': len(self.chain)+1,
+                'TimeStamp': datetime.datetime.now(),
+                'proof': proof, 
+                'previous_hash': previous_hash,
+                'data':"Dummy data"
+            }
+        self.chain.append(block)
+        return block
+
+    def get_previousblock(self):
+        return self.chain[-1]
         
+
 
 
 #Mining the block 
